f6a1f1fdc1e60f2e408e0ab2a2e4c64c0255fc43
Stephen Essoun <boit7845@gmail.com>
2025-01-03 21:28:07 +0000

    able to customise primary colour

---
+++ lib/src/settings/settings.dart
@@ -0,0 +1,108 @@
+import 'package:ch_db_admin/src/main_view/presentation/menu_tile.dart';
+import 'package:ch_db_admin/theme/apptheme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class SettingsView extends StatefulWidget {
+  const SettingsView({super.key});
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+// Define preset colors
+  final List<Color> presetColors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.orange,
+    Colors.purple,
+    Colors.teal,
+    Colors.pink,
+    Colors.indigo,
+    Colors.yellow,
+    Colors.brown,
+  ];
+
+  void _showThemeDialog(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Choose Theme'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SwitchListTile(
+                title: const Text('Dark Mode'),
+                value: themeProvider.isDarkMode,
+                onChanged: (value) {
+                  themeProvider.toggleTheme();
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text('Choose Primary Color:'),
+              const SizedBox(height: 10),
+              Wrap(
+                spacing: 10.0,
+                children: presetColors.map((color) {
+                  return GestureDetector(
+                    onTap: () async {
+                      themeProvider.setPrimaryColor(color);
+                      // Save preferences after selecting the color and theme
+                      await savePreferences(themeProvider.isDarkMode,
+                          themeProvider.theme.primaryColor);
+
+                      // Close the dialog after saving
+                      Navigator.of(context).pop();
+                    },
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: color,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: themeProvider.theme.primaryColor == color
+                              ? Colors.black
+                              : Colors.transparent,
+                          width: 3.0,
+                        ),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            MenuTile(
+              title: 'Change App Colour',
+              showTrailingIcon: false,
+              onTap: () {
+                _showThemeDialog(context);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
